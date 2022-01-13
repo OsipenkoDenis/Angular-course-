39a9e2b32015810fa0266f136ec835e92328db45
osipenkoDenis <denis.osipenko@inbox.ru>
2022-01-14 00:12:35 +0300

    to add error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const routes: Routes = [
   {path:"heroes", component:HeroesComponent},
   {path:"dashboard", component: DashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {path:"detail/:id", component: HeroDetailComponent},
+  {path:"error", component:ErrorPageComponent},
+  {path:"**", redirectTo:"/error"},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
